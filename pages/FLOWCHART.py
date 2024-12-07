122db9629711459564ae9c3924899ed8a8001868
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2024-12-07 19:30:18 +0530

    SUVAJIT & PRIYA DHARSHINI G | >TeamMan&Woman | RTFD | Final project ready

--- pages/FLOWCHART.py
+++ pages/FLOWCHART.py
@@ -8,8 +8,8 @@ stream.set_page_config(page_title="FLOWCHART", page_icon=":eye", layout="wide")
 
 
 def main():
-      stream.header("FLOW OF EXECUTION OF THE APPLICATION")
-      flow_execution_image = 'Images/FLOW-OF-EXECUTION.png'
+      # stream.header("FLOW OF EXECUTION OF THE APPLICATION")
+      flow_execution_image = 'Images/ARCHITECTURE.png'
       stream.image(flow_execution_image, width=850)
 
 
